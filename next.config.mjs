3699e3b08c6c87915ace6e29d35e2c2de660d6b2
brokencygnus <azmyadzkiansyah@gmail.com>
2024-04-21 21:00:31 +0700

    Fixed base url not redirecting to home

--- next.config.mjs
+++ next.config.mjs
@@ -3,7 +3,7 @@ const nextConfig = {
     async redirects() {
     return [
         {
-        source: "/",
+        source: "/page",
         destination: "/home",
         permanent: true,
         },
